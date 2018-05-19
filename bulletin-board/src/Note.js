155f648d01450bd186b8525910e182e2c3b769f1
sukhwinder kaur <s.kaur12389@gmail.com>
2018-05-19 16:05:43 +0200

    2.1 Added Functions without Functionality

--- bulletin-board/src/Note.js
+++ bulletin-board/src/Note.js
@@ -1,30 +1,66 @@
 import React, { Component } from 'react'
 import FaPencil from 'react-icons/lib/fa/pencil'
 import Fatrash from 'react-icons/lib/fa/trash'
+import FaFloppyO from 'react-icons/lib/fa/floppy-o'
+
 class Note extends Component{
     constructor (props){
         super(props)
+        this.state = {
+            editing: false
+        }
         this.edit = this.edit.bind(this)
         this.remove = this.remove.bind(this)
+        this.renderForm = this.renderForm.bind(this)
+        this.renderDisplay = this.renderDisplay.bind(this)
+        this.save = this.save.bind(this)
+
 
     }
     edit(){
-        alert('editing note')
+        this.setState(
+            {
+                editing: true
+            })
     }
     remove()
     {
         alert('remove note')
     }
-    render () {
+    save(){
+        alert('saved')
+    }
+
+    renderForm(){
+        return(
+            <div className= "note">
+            <form>
+            <textarea />
+            <button onClick ={this.save}><FaFloppyO /></button>
+
+            </form>
+            </div>
+        )
+    }
+    renderDisplay () {
         return(
             <div className="note">
                 <p>learn</p>
                 <span>
                     <button onClick={this.edit} id = "edit"><FaPencil />  </button>
                     <button onClick={this.remove} id ="remove"><Fatrash /> </button>
+
                 </span>
             </div>
         )
+    }
+    render() {
+        return this.state.editing ? this.renderForm() : this.renderDisplay()
+        /*if(this.state.editing) {
+            return this.renderForm()
+        }else {
+            return this.renderDisplay()
+        }*/
     }
 }
 
