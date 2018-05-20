ccb110e458df57482e70a7648aefce87a519995f
sukhwinder kaur <s.kaur12389@gmail.com>
2018-05-20 22:34:58 +0200

    Created stateless functional Component
    there are three ways to create component.
    1. createClass
    2. ES6Class
    3. Stateless functional Component

--- sample/src/components/SkiDayCount.js
+++ sample/src/components/SkiDayCount.js
@@ -1,36 +1,33 @@
-import {Component} from 'react'
+
 import '../stylesheets/ui.scss'
 
-export class SkiDayCount extends Component{
 
-    percentToDecimal(decimal){
-        return((decimal*100)+ '%')
+const percentToDecimal = (decimal) => {
+         return((decimal*100)+ '%')
     }
-    calcGoalProgress(total,goal){
-        return this.percentToDecimal(total/goal)
+    const calcGoalProgress = (total,goal) => {
+        return percentToDecimal(total/goal)
     }
-    render() {
-        return (
+    export const SkiDayCount = ({total,powder,backcountry,goal}) => (
             <div className="ski-day-count">
                <div className="total-days">
-                 <span>{this.props.total}</span>
+                 <span>{total}</span>
                   <span>days</span>
             </div>
             <div className="powder-days">
-            <span>{this.props.powder}</span>
+            <span>{powder}</span>
             <span>days</span>
             </div>
             <div className="backcountry-days">
-            <span>{this.props.backcountry}</span>
+            <span>{backcountry}</span>
             <span>hiking day</span>
 
             </div>
             
             <div>
-                <span>{this.calcGoalProgress(this.props.total,
-                this.props.goal)}</span>
+                <span>{calcGoalProgress(total,
+                goal)}</span>
                 </div>
                 </div>
         )
-    }
-}
+    
