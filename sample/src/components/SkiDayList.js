873b36660933f4da68f6f84c40eda855e57d16a2
sukhwinder kaur <s.kaur12389@gmail.com>
2018-05-21 14:57:00 +0200

    9.2 navigating the menu using routing parameters
    here we used routing parameters to change the m enu

--- sample/src/components/SkiDayList.js
+++ sample/src/components/SkiDayList.js
@@ -3,8 +3,14 @@ import SnowFlake from 'react-icons/lib/ti/weather-snow'
 import Calendar from 'react-icons/lib/fa/calendar'
 import {SkiDayRow} from './SkiDayRow'
 import { PropTypes } from 'react'
+import { Link } from 'react-router'
 
-export const SkiDayList = ({days}) => (
+export const SkiDayList = ({days, filter}) => {
+    const filteredDays =(!filter || !filter.match(/powder|backcountry/)) ? days:
+          days.filter(day => day[filter])
+
+    return (
+        <div className="ski-day-list">
     <table>
         <thead>
             <tr>
@@ -13,9 +19,22 @@ export const SkiDayList = ({days}) => (
                 <th>Powder</th>
                 <th>Backcountry</th>
             </tr>
+            <tr>
+                <td colSpan={4}>
+                <Link to="/list-days">
+                    all days
+                </Link>
+                <Link to="/list-days/powder">
+                    powder days
+                </Link>
+                <Link to="/list-days/backcountry">
+                    backcountry days
+                </Link>
+                </td>
+            </tr>
         </thead>
         <tbody>
-            {days.map((day , i) =>
+            {filteredDays.map((day , i) =>
             <SkiDayRow key= {i}
            {...day} />
             )}
@@ -23,14 +42,14 @@ export const SkiDayList = ({days}) => (
 
 
     </table>
-
+</div>
 )
-
+}
 SkiDayList.propTypes = {
     days: function(props){
         if(!Array.isArray(props.days)){
             return new Error(
-                "SkiDayList should eb an array"
+                "SkiDayList should be an array"
             )
         }
         else if (!props.days.length){
--- sample/src/index.js
+++ sample/src/index.js
@@ -4,6 +4,7 @@ import './stylesheets/ui.scss'
 import { App } from './components/App'
 import {Whoops404} from './components/Whoops404'
 import { Router , Route , hashHistory} from 'react-router'
+import {SkiDayList} from './components/SkiDayList'
 
 window.React = React
 
@@ -11,7 +12,9 @@ window.React = React
 render(
 	<Router history={hashHistory}>
     <Route path="/" component={App}/>
-    <Route path= "list-days" component={App}/>
+    <Route path= "list-days" component={App}>
+    <Route path=":filter" component={App}/>
+    </Route>
     <Route path= "add-day" component={App}/>
     <Route path="*" component={Whoops404}/>
   </Router>, 
